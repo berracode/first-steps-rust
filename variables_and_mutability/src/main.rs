0db7f0fc70072424dc98b7da11cce45d3ed39cb3
berracode <juanguillermoalarcon@gmail.com>
2022-12-15 12:11:12 -0500

    shadowing and inner scope. Shadowing is different than mut

--- variables_and_mutability/src/main.rs
+++ variables_and_mutability/src/main.rs
@@ -1,4 +1,20 @@
 fn main() {
+
+    //shadowing: you can declare a new variable with the same name as a previous variable.
+
+    let x = 5;
+
+    let x = x + 1; //the compiler will see this x variable because is the shadow
+
+    { //inner scope starts
+        let x = x * 2; //now, in inner scope, this x variable is what the compiler will see, but just in this inner scope
+        println!("The value of x in the inner scope is: {x}");
+    } //inner scope ends
+
+    println!("The value of x is: {x}");
+}
+
+fn main1() {
     let mut x = 5; // in rust by default variables are immutable. With mut after let, our variable is mutable now.
     println!("The value of x is: {x}");
     x = 6; // x = 6 will throw an error.
