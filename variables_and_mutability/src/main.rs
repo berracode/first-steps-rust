b4933b26cbcdf8f33d31e65af09c05b8c2e7f597
berracode <juanguillermoalarcon@gmail.com>
2022-12-15 12:01:16 -0500

    by default variables are immutable in rust

--- variables_and_mutability/src/main.rs
+++ variables_and_mutability/src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5; // in rust by default variables are immutable. With mut after let, our variable is mutable now.
+    println!("The value of x is: {x}");
+    x = 6; // x = 6 will throw an error.
+    println!("The value of x is: {x}");
 }
