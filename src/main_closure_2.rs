d6d22d39c09441c435a612bda38eaef4dca8a6e6
QuantumInformatik <juanguillermoalarcon@gmail.com>
2022-12-18 18:16:23 -0500

    I hace studied a bit more about closure

---
+++ src/main_closure_2.rs
@@ -0,0 +1,147 @@
+/*
+Functional Language Features: Iterators and Closures
+*/
+
+use std::thread;
+
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum ShirtColor {
+    Red,
+    Blue,
+}
+
+struct Inventory {
+    shirts: Vec<ShirtColor>,
+}
+
+impl Inventory {
+    fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
+        user_preference.unwrap_or_else(|| self.most_stocked())
+    }
+
+    fn most_stocked(&self) -> ShirtColor {
+        let mut num_red = 0;
+        let mut num_blue = 0;
+
+        for color in &self.shirts {
+            match color {
+                ShirtColor::Red => num_red += 1,
+                ShirtColor::Blue => num_blue += 1,
+            }
+        }
+        if num_red > num_blue {
+            ShirtColor::Red
+        } else {
+            ShirtColor::Blue
+        }
+    }
+}
+
+fn basic_closure(){
+    let store = Inventory {
+        shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue],
+    };
+
+    let user_pref1 = Some(ShirtColor::Red);
+    let giveaway1 = store.giveaway(user_pref1);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref1, giveaway1
+    );
+
+    let user_pref2 = None;
+    let giveaway2 = store.giveaway(user_pref2);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref2, giveaway2
+    );
+}
+
+fn same_clousure_different_type(){
+    let example_closure = |x| x;
+
+    let s = example_closure(String::from("hello"));
+    //let n = example_closure(5);
+}
+
+fn capturing_references_or_moving_ownership_immutable_reference(){
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+
+    let only_borrows = || println!("From closure: {:?}", list);
+
+    println!("Before calling closure: {:?}", list);
+    only_borrows();
+    println!("After calling closure: {:?}", list);
+}
+
+fn capturing_references_or_moving_ownership_mutable_reference(){
+    let mut list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+
+    let mut borrows_mutably = || list.push(7);
+
+    borrows_mutably();
+    println!("After calling closure: {:?}", list);
+}
+
+fn capturing_references_or_moving_ownership_taking_ownership(){
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+
+    thread::spawn(move || println!("From thread: {:?}", list))
+        .join()
+        .unwrap();
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn capturing_references_or_moving_ownership_fn_traits(){
+    let mut list = [
+        Rectangle { width: 10, height: 1 },
+        Rectangle { width: 3, height: 5 },
+        Rectangle { width: 7, height: 12 },
+    ];
+
+    list.sort_by_key(|r| r.width);
+
+    /*
+    That line is like those line in java lang: 
+
+    list.forEach((r)->System.out.println(r))
+    
+    */
+
+    //println!("{:#?}", list);
+}
+
+fn capturing_references_or_moving_ownership_fn_traits_num(){
+    let mut list = [
+        Rectangle { width: 10, height: 1 },
+        Rectangle { width: 3, height: 5 },
+        Rectangle { width: 7, height: 12 },
+    ];
+
+    let mut num_sort_operations = 0;
+    list.sort_by_key(|r| {
+        num_sort_operations += 1;
+        r.width
+    });
+    println!("{:#?}, sorted in {num_sort_operations} operations", list);
+}
+
+fn main() {
+    //basic_closure();
+    //same_clousure_different_type();
+    //capturing_references_or_moving_ownership_immutable_reference();
+    //capturing_references_or_moving_ownership_mutable_reference();
+    //capturing_references_or_moving_ownership_taking_ownership();
+
+    //capturing_references_or_moving_ownership_fn_traits();
+    capturing_references_or_moving_ownership_fn_traits_num();
+   
+}
